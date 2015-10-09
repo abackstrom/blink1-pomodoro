f3ac2b16b7de9f609b0d88b7ab257f302b10e44e
Annika Backstrom <annika@sixohthree.com>
2015-10-09 13:01:30 -0400

    pomodoro: copy over some of the tomatoes changes

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # makes blink(1) turn red for $pomodoro_length minutes and then green for $break_length minutes
 
-blink_path=".././blink1-tool"
+blink_path=$(which blink1-tool)
+if [ -z "$blink_path" ] ; then
+	blink_path="../blink1-tool"
+fi
 
 pomodoro_color="255,0,0"
-
 break_color="0,255,0"
 
 pomodoro_length=25 # length of tomato in minutes
@@ -13,7 +15,19 @@ break_length=5 # length of break in minutes
 
 let pomodoro_secs=$pomodoro_length*60
 let break_secs=$break_length*60
-let total_length=$pomodoro_length+$break_length
+
+function setColor {
+	$blink_path --rgb $1 --blink 4 > /dev/null 2>&1
+	$blink_path --rgb $1 > /dev/null 2>&1
+}
+
+function setWork {
+	setColor $pomodoro_color
+}
+
+function setRest {
+	setColor $break_color
+}
 
 pomodoro=0
 
@@ -21,10 +35,8 @@ while true; do
 	echo "$pomodoro_secs"
 	let pomodoro+=1
 	echo "Now beginning pomodoro # $pomodoro"
-	$blink_path --rgb $pomodoro_color --blink 4 > /dev/null 2>&1
-	$blink_path --rgb $pomodoro_color > /dev/null 2>&1
+	setWork
 	sleep $pomodoro_secs
-	$blink_path --rgb $break_color --blink 4 > /dev/null 2>&1
-	$blink_path --rgb $break_color > /dev/null 2>&1
+	setRest
 	sleep $break_secs
 done
