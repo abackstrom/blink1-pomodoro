5eb336f3498c050af58e6dd5f86c4f438115bf87
Annika Backstrom <annika@sixohthree.com>
2015-10-09 13:00:12 -0400

    tomatoes: sleep until the next state change

--- tomatoes.sh
+++ tomatoes.sh
@@ -1,42 +1,53 @@
 #!/bin/bash
 # makes blink(1) turn red for 25 minutes (starting on the hour and half-hour) and then green for five minutes
 
-blink_path=".././blink1-tool"
-
-tomato_color="255,0,0"
-
-break_color="0,255,0"
-
-# initial color:
-minute=$(date +"%M")
-minute=`echo $minute|sed 's/^0*//'`
-if [[ "$minute" -lt 25 || "$minute" -ge 30 && "$minute" -lt 55 ]]
-then
-	$blink_path --rgb $tomato_color > /dev/null 2>&1
-else
-	$blink_path --rgb $break_color > /dev/null 2>&1
+blink_path=$(which blink1-tool)
+if [ -z "$blink_path" ] ; then
+	blink_path="../blink1-tool"
 fi
 
-# change colors and beep:
-while true; do
-	minute=$(date +"%M")
-	minute=`echo $minute|sed 's/^0*//'`
-	second=$(date +"%S")
-	second=`echo $second|sed 's/^0*//'`
-	if [[ "$second" -lt 5 ]]
-	then
-		if [[ "$minute" -eq 0 || "$minute" -eq 30 ]]
-		then
-			paplay /usr/share/sounds/KDE-Im-Highlight-Msg.ogg # beep
-			$blink_path --rgb $tomato_color --blink 4 > /dev/null 2>&1
-			$blink_path --rgb $tomato_color > /dev/null 2>&1
-		elif [[ "$minute" -eq 25 || "$minute" -eq 55 ]]
-		then
-			paplay /usr/share/sounds/KDE-Im-Highlight-Msg.ogg # beep
-			$blink_path --rgb $break_color --blink 4 > /dev/null 2>&1
-			$blink_path --rgb $break_color > /dev/null 2>&1
-		fi
-		sleep 5 # loop runs every 5 seconds
+tomato_color="255,0,0"
+break_color="0,255,0"
+
+paplay=$(which paplay)
+
+function doBell {
+	if [ ! -z "$paplay" ] ; then
+		paplay /usr/share/sounds/KDE-Im-Highlight-Msg.ogg # beep
 	fi
-	sleep 5 # loop runs every 5 seconds
+}
+
+function setColor {
+	$blink_path --rgb $1 --blink 4 > /dev/null 2>&1
+	$blink_path --rgb $1 > /dev/null 2>&1
+}
+
+function setWork {
+	doBell
+	setColor $tomato_color
+}
+
+function setRest {
+	doBell
+	setColor $break_color
+}
+
+# change colors and beep:
+while true ; do
+	minutes=$(printf %d $(date +%M))
+	seconds=$(printf %d $(date +%S))
+	remaining=$((1800 - ($minutes * 60 + $seconds) % 1800))
+	echo "remaining is: $remaining"
+	if [ $remaining -gt 300 ] ; then
+		setWork
+		echo "setWork ; sleeping for" $(($remaining - 300))
+		sleep $(($remaining - 300))
+		setRest
+		echo "setRest ; forcing sleep for 300"
+		sleep 300
+	else
+		setRest
+		echo "setRest ; sleeping for $remaining"
+		sleep $remaining
+	fi
 done
